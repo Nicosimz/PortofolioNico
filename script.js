41f2402b8a8e3b19d8ece4238f13c4e2a039ddb2
Nicosimz <151869449+Nicosimz@users.noreply.github.com>
2024-01-04 21:41:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var fadeElement = document.querySelector('.fade-in-element');
 
     // Function to check if an element is in the viewport
-    function isInViewport(element) {
-        var rect = element.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
+   
 
     // Function to handle scroll events
     function handleScroll() {
